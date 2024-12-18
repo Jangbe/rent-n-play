301e52ac25794b9c2fcae2f53d2e7b49a06d0c9c
Jangbe <jangebe91@gmail.com>
2024-12-18 17:56:59 +0700

    dsadasda

--- resources/js/plugins/router.js
+++ resources/js/plugins/router.js
@@ -5,7 +5,7 @@ import routes from '~pages';
 const isUserLoggedIn = () => !!(localStorage.getItem('accessToken'))
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHistory(),
     routes: setupLayouts(routes),
 })
 router.beforeEach(to => {
